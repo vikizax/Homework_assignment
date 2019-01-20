c480a145ed891d62de31099926340fbe5131662c
vikizax <44132243+vikizax@users.noreply.github.com>
2019-01-20 13:41:59 +0530

    Create function.js

---
+++ JavaScript_ES6_Homework/function.js
@@ -0,0 +1,73 @@
+/*
+ Homework Assignment #4 
+ Functions
+ */
+
+ /*
+ All men are mortal
+ Socrates is a man
+ Therefore, socrates is mortal.
+ */
+
+ 
+const namesArray = ["Socrates","Nicolas Flamel","Zusudra"];
+
+const isMortal = (name) => {
+    if(name === ("Socrates" || "Plato")){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+namesArray.forEach((name) => {
+    console.log(name + " is mortal ?: " + isMortal(name));
+});
+
+console.log("\n");
+
+/*--------------------------------Cake Flavor!------------------------------- */
+
+//cakes we have
+const cakes = ["vanilla", "chocolate", "lemon"];
+
+//function exp to check cake is chocolate, it will return boolean array
+const checkChocolate = (cakeArr) => {
+    const isChocolate = [] ;
+    //our cake will be checked,if chocolate then push true else false
+    cakeArr.forEach((n) => {
+        if(n === "chocolate") {
+            isChocolate.push(true);
+        } else {
+            isChocolate.push(false);
+        }
+    });
+    return isChocolate;
+};
+
+//function exp to Check all the cakes we have and return the actual flavor
+//1. An array of all cake possibilities.
+//2. A boolean representing whether or not the cake is chocolate.
+const checkAllCakes = (cakeArr, checkChoco) => {
+    checkChoco.forEach((n, i) => {
+        if(n){
+            console.log("This cake is chocolate!!");
+        }
+        else{
+            console.log("This cake is not chocolate, it is " + cakeArr[i]);
+        }
+    })
+}
+checkAllCakes(cakes, checkChocolate(cakes));
+
+
+
+
+
+
+
+ /*
+This cake is either vanilla or chocolate.
+This cake is not chocolate.
+Therefore, this cake is vanilla.
+*/
