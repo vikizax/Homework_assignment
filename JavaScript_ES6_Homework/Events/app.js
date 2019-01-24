2a3dea9727bf148b8731d6a6b44921f3d1f62a3f
vikizax <44132243+vikizax@users.noreply.github.com>
2019-01-25 01:01:30 +0530

    Update app.js

--- JavaScript_ES6_Homework/Events/app.js
+++ JavaScript_ES6_Homework/Events/app.js
@@ -1,4 +1,4 @@
-//global
+//global 
 const boardContainer = document.getElementById("container");
 const cells = boardContainer.querySelectorAll(".square");
 const resetButton = document.getElementById("reset");
@@ -15,31 +15,37 @@ const winPattern =[
     [0,4,8],
     [2,4,6]
 ];
-console.log(winPattern[3][1]);
 //0 means playerOne and 1 means playerTwo
 let activePlayer = 0;
-
-//3 means not played
+//2 means not played
+//place on the tic-tac-toe board
 let gameState = [2,2,2,2,2,2,2,2,2];
-
 //will change to false if player has won
 let gameIsActive = true;
 
+
+//Function exp to reset the game
 const resetAll = () => {
+    //reset the tic-tac-toe board place as 2
     gameState = [2,2,2,2,2,2,2,2,2];
+    //set the default start player as 0
     activePlayer = 0;
+    //set game is active true
     gameIsActive = true;
+    //reset all the cells of the tic-tac-toe board as default
     for(const prop of cells) {
         prop.style.pointerEvents = "auto";
         prop.style.color = "rgba(255, 255, 255, 0)";
         prop.classList.remove("playerOne")
         prop.classList.remove("playerTwo");
-        console.log(prop);
-}
+    }
 }
 
+//Function exp to display the dialog window to show the game end stats
 const displayWinner = (n)=> {
+    //set delay for animation to complete 
     setTimeout(()=> {
+        //check n value 0 for X, 1 for O, or cats game
         if(n === 0) {
             alert("X has won!");
         } else if(n === 1) {
@@ -47,58 +53,81 @@ const displayWinner = (n)=> {
         } else{
             alert("Cats game!");     
         }
+        //once game ends reset the progress
         resetAll();
     }, 400);
 }
 
+//Function exp to switch between X and O player, X start as first by default
 const playerSet = (p)=> {
+    //checks position on the board not played and game is active
     if(gameState[p] === 2 && gameIsActive){
+        //0 for X
         if(activePlayer === 0){
+            //set the position played on the board as 0, 'X'
             gameState[p] = 0;
+            //change the active player to 1, '0'
             activePlayer = 1;
         } else {
+            //set the position played on the board as 1, 'O'
             gameState[p] = 1;
+            //change the active player to 0, 'X'
             activePlayer = 0;
         }
+        //check the win condition to get end game result
         checkWinner();
     }
 }
+
+//Function exp to get the winner, end game result 
 const checkWinner = () => {
+    //check for game is active or not
     if(gameIsActive){
+        //loop to check the winning pattern
         for(let i = 0; i < winPattern.length; i++){
+            //check tic-tac-toe played position and compare it with winning patterns
             if(gameState[winPattern[i][0]] === 0 
                 && gameState[winPattern[i][1]] === 0
                 && gameState[winPattern[i][2]] === 0 
                 && gameState[winPattern[i][0]] !== 2){
-                    console.log(i, " i am here in player 1");
+                    //if winner found change the game is active to false
                     gameIsActive = false;
+                    //show the result 
                     displayWinner(0);
                     return;
                 } else if(gameState[winPattern[i][0]] === 1 
                     && gameState[winPattern[i][1]] === 1
                     && gameState[winPattern[i][2]] === 1 
                     && gameState[winPattern[i][0]] !== 2){
-                        console.log(i, " i am here in player 2");
                         gameIsActive = false;
                         displayWinner(1);
                         return;
                 }
             }
+    //check for draw or cats game        
     if(gameIsActive){
+        //assume game over true
         let gameIsOver = true;
+        //loop to check the positions on board played
         for(const prop of gameState) {
+            //check for not played positions
             if(prop === 2) {
+                //set game over false, position still left to play
                 gameIsOver = false;
                 }                        
             }
+            //check game over true
             if(gameIsOver) {
+                //set game active false
                 gameIsActive = false;
+                //display result, cats game
                 displayWinner();
                 }
         }
     }
 }
 
+//Function exp to setup the cells 
 const play = (e) => {
     const place = e.target;
     const plays = place.id;
@@ -121,10 +150,11 @@ const play = (e) => {
         }
     }
 }
-
+//add event event listeners to all the cells
 for(const prop of cells) {
         prop.addEventListener("click", play);
 }
+//add event listener to New Game button to rest the game
 resetButton.addEventListener("click", resetAll);
 
 
